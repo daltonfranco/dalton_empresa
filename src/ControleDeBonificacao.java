b2b79018a98ced48d261db7acd8c43e746bfecd7
daltonfranco <daltonpacolafranco.git@hotmail.com>
2021-09-23 15:14:56 -0300

    alterações para a p1 java

--- src/ControleDeBonificacao.java
+++ src/ControleDeBonificacao.java
@@ -1,14 +1,14 @@
-public class ControleDeBonificacao {
+public class ControleDeBonificacao { //Criação da classe publica ControleDebonificação, abrindo com a cheve
     
-    private double totalDeBonus;
+    private double totalDeBonus; //criacao do Vetor totalDeBonus privado do tipo double
 
-    public double getTotalDeBonus(){
-        return this.totalDeBonus;
-    }
+    public double getTotalDeBonus(){ // criacao do método getTotalDeBonus do tipo double, abrindo com a chave
+        return this.totalDeBonus; //retorno do vetor totalDebonus
+    }// Fechando o método com a chave
 
-    public double calculadoraTotalDeBonus(Funcionario f){
-        return this.totalDeBonus = this.totalDeBonus + f.bonifica();
-    }
+    public double calculadoraTotalDeBonus(Funcionario f){ // Criacao do método publico calculadoraTotalDeBonus, atribuindo a classe funcionario com o objeto f, abindo com a chave
+        return this.totalDeBonus = this.totalDeBonus + f.bonifica(); //retorno do vetor totalDeBonus, recebendo o vetor totalDeBonus + o método bonifica do objeto f
+    }//fechando o método com a chave
 
     
-}
+}//fechando a classe com a chave
--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,85 +1,85 @@
-public abstract class Funcionario {
+public abstract class Funcionario { // criação da classe publica e abstrata Funcionário, abrindo com a chave
 
-    protected String nome;
-    protected String departamento;
-    protected double salario;
-    protected String  dataDeEntrada;
-    protected String rg;
-    protected boolean estaNaEmpresa;
+    protected String nome; // criacao do vetor protegido nome do tipo String
+    protected String departamento;// criacao do vetor protegido departamento da tipo String
+    protected double salario;// criacao do vetor protegido salario do tipo double
+    protected String dataDeEntrada;// criacao do vetor protegido dataDeEntrada do tipo String
+    protected String rg;// criacao do vetor protegido rg do tipo String
+    protected boolean estaNaEmpresa;// criacao do vetor protegido estaNaEmpresa do tipo boolean
 
-    public String getNome(){
-        return nome;
-    }
+    public String getNome(){// criacao do método getNome do tipo String, abrindo com a chave
+        return nome;// retorno do vetor nome
+    }// fechando o metodo com a chave
 
-    public void setNome(String nome){
-        this.nome = nome;
-    }
+    public void setNome(String nome){// criação do método publico e vazio setNome, recebendo um parametro nome do tipo String, abrindo com a chave
+        this.nome = nome;// vetor nome recebendo o parametro nome
+    }// fechando o metodo com a chave
 
-    public String getDepartamento(){
-        return departamento;
-    }
+    public String getDepartamento(){// criacao do método getDepartamento do tipo String, abrindo com a chave
+        return departamento;// retorno do vetor departamento
+    }// fechando o metodo com a chave
 
-    public void setDepartamento(String departamento){
-        this.departamento = departamento;
-    }
+    public void setDepartamento(String departamento){// criação do método publico e vazio setDepartamento, recebendo um parametro departamento do tipo String, abrindo com a chave
+        this.departamento = departamento;// vetor departamento recebendo o parametro departamento
+    }// fechando o metodo com a chave
 
-    public double getSalario(){
-        return salario;
-    }
+    public double getSalario(){// criacao do método getSalario do tipo Double, abrindo com a chave
+        return salario;// retorno do vetor salario
+    }// fechando o metodo com a chave
 
-    public void setSalario(double salario){
-        this.salario = salario;
-    }
+    public void setSalario(double salario){// criação do método publico e vazio setSalario, recebendo um parametro salario do tipo double, abrindo com a chave
+        this.salario = salario;// vetor salario recebendo o parametro salario
+    }// fechando o metodo com a chave
 
-    public String getDataDeEntrada(){
-        return dataDeEntrada;
-    }
+    public String getDataDeEntrada(){// criacao do método getDataDeEntrada do tipo String, abrindo com a chave
+        return dataDeEntrada;// retorno do vetor dataDeEntrada
+    }// fechando o metodo com a chave
 
-    public void setDataDeEntrada(String dataDeEntrada){
-        this.dataDeEntrada = dataDeEntrada;
-    }
+    public void setDataDeEntrada(String dataDeEntrada){// criação do método publico e vazio setDataDeEntrada, recebendo um parametro dataDeEntrada do tipo String, abrindo com a chave
+        this.dataDeEntrada = dataDeEntrada;// vetor dataDeEntrada recebendo o parametro dataDeEntrada
+    }// fechando o metodo com a chave
 
-    public String getRg(){
-        return rg;
-    }
+    public String getRg(){// criacao do método getRg do tipo String, abrindo com a chave
+        return rg;// retorno do vetor rg
+    }// fechando o metodo com a chave
 
-    public void setRg(String rg){
-        this.rg = rg;
-    }
+    public void setRg(String rg){// criação do método publico e vazio setRg, recebendo um parametro rg do tipo String, abrindo com a chave
+        this.rg = rg;// vetor rg recebendo o parametro rg
+    }// fechando o metodo com a chave
 
-    public boolean getEstaNaEmpresa(){
-        return estaNaEmpresa;
-    }
+    public boolean getEstaNaEmpresa(){// criacao do método getEstaNaEmpresa do tipo boolean, abrindo com a chave
+        return estaNaEmpresa;// retorno do vetor estaNaEmpresa
+    }// fechando o metodo com a chave
 
-    public void setEstaNaEmpresa(boolean estaNaEmpresa){
-        this.estaNaEmpresa = estaNaEmpresa;
-    }
+    public void setEstaNaEmpresa(boolean estaNaEmpresa){// criação do método publico e vazio setEstaNaEmpresa, recebendo um parametro estaNaEmpresa do tipo boolean, abrindo com a chave
+        this.estaNaEmpresa = estaNaEmpresa;// vetor estaNaEmpresa recebendo o parametro estaNaEmpresa
+    }// fechando o metodo com a chave
 
-    public void calculaSalario(double valor){
-        this.salario = valor - (valor * 0.06);
-    }
+    public void calculaSalario(double valor){// criacao do metodo publico e vazio calculaSalario, abrituindo o parametro valor do tipo double, abindo com a chave
+        this.salario = valor - (valor * 0.06);// vetor salario recebendo o parametro valor - (parametro valor * 0.06)
+    }// fechando o metodo com a chave
 
-    public void calculaSalario(double valor, double horaExtra){
-        double valorHorasExtra = (valor/30) / 8 * horaExtra;
-        this.salario = valor - (valor*0.06) + valorHorasExtra;
-    }
+    public void calculaSalario(double valor, double horaExtra){// criação do metodo publico e vazio calculaSalario, atribuindo o parametro valor e horaExtra, ambas do tipo double, abindo com a chave
+        double valorHorasExtra = (valor/30) / 8 * horaExtra;// vetor valorHorasExtra recebendo (parametro valor/30) / 8 * parametro horaExtra
+        this.salario = valor - (valor*0.06) + valorHorasExtra;// vetor salario recebendo o parametro valor - (parametro valor*0,06) + veto valorHorasExtras
+    }// fechando o metodo com a chave
 
-    public double calculaSalario() {
-        return this.salario = this.salario - (this.salario * 0.06);
-    }
+    public double calculaSalario() {// criacao do metodo publico calculaSalario do tipo double, abrindo com a chave
+        return this.salario = this.salario - (this.salario * 0.06);// retorno do vetor salario recebendo o vetor salario - (vetor salario * 0.06)
+    }// fechando o metodo com a chave
 
-    public double bonifica(){
-        return this.salario * 0.10;
-    }
+    public double bonifica(){// criação do metodo publico bonifica do tipo double, abrindo com a chave
+        return this.salario * 0.10;// retorno do vetor salario * 0.10
+    }// fechando o metodo com a chave
 
-    public void demite(){
-        estaNaEmpresa = false;
-    }
+    public void demite(){// criação do metodo publico e vazio demite, abrindo com a chave
+        estaNaEmpresa = false;// vetor estaNaEmpresa recebendo o false
+    }// fechando o metodo com a chave
 
-    public double calculaGanhaAnual(){
-        return salario * 12;
-    }
+    public double calculaGanhaAnual(){// criação do método publico calculaGanhaAnual do tipo double, abrindo com a cheve
+        return salario * 12;// retorno do vetor salario * 12
+    }// fechando o metodo com a chave
 
     
     
-}
+}// fechando a classe com a chave
